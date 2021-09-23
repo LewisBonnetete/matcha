ff185e4d8e97ab0b5a2584de1c26276c21e14027
Lewis BONNETETE <lbonnete@e1r8p21.42.fr>
2021-09-23 11:30:42 +0200

    init

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          Matcha
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Stalker" />
+        <q-route-tab to="/page2" label="Profile" />
+        <q-route-tab to="/page3" label="Settings" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Matcha</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
